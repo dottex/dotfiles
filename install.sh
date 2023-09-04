07d475f6f6a20449e543c63ef5ea328145db12d6
JHH <j@dorothy-vaughn>
2023-09-04 16:56:42 +0700

    cp command with global path.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #Install nvim as a feature
 sudo apt update
 sudo apt install taskwarrior -y
-cp task/.config/task/taskrc ~/.taskrc
+cp ~/dotfiles/task/.config/task/taskrc ~/.taskrc
 git clone https://github.com/wakatime/vim-wakatime ~/.local/share/nvim/site/pack/plugins/start/wakatime --depth=1
 
 
