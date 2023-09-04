06729e937c7c98e65a3bd61d52e1398355de9051
JHH <j@dorothy-vaughn>
2023-09-04 16:36:48 +0700

    Removed the background flag so we can see the installation process with apt

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #Install nvim as a feature
-sudo apt install taskwarrior -y &
+sudo apt install taskwarrior -y
 cp task/.config/task/taskrc ~/.taskrc
 git clone https://github.com/wakatime/vim-wakatime ~/.local/share/nvim/site/pack/plugins/start/wakatime --depth=1
 
