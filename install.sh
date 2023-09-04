5f5375bf7ab012783f1e2e7001bd4c01bf198766
JHH <j@dorothy-vaughn>
2023-09-04 16:30:50 +0700

    Added taskwarrior installation.

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-#Install nvim and taskwarrior with the usual tools
+#Install nvim as a feature
+sudo apt install taskwarrior -y &
 cp task/.config/task/taskrc ~/.taskrc
 git clone https://github.com/wakatime/vim-wakatime ~/.local/share/nvim/site/pack/plugins/start/wakatime --depth=1
 
