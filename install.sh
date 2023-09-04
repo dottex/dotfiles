26772b3f7333f8c12d356315f0f43c8979b531fd
JHH <j@dorothy-vaughn>
2023-09-04 16:03:46 +0700

    Updated installation file.

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
-sudo apt install stow -y
+#Install nvim and taskwarrior with the usual tools
+cp task/.config/task/taskrc ~/.taskrc
+git clone https://github.com/wakatime/vim-wakatime ~/.local/share/nvim/site/pack/plugins/start/wakatime --depth=1
+
 
