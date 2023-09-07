61804bdf048880921560e822f113c63189016382
JHH <j@dorothy-vaughn>
2023-09-07 18:38:42 +0700

    Installed wakatime in vim also.

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ sudo apt update
 sudo apt install taskwarrior -y
 cp ~/dotfiles/task/.config/task/taskrc_example ~/.taskrc
 git clone https://github.com/wakatime/vim-wakatime ~/.local/share/nvim/site/pack/plugins/start/wakatime --depth=1
+git clone https://github.com/wakatime/vim-wakatime ~/.vim/pack/plugins/start/wakatime --depth=1
 
 
