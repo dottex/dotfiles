281625f3a574cdb93cc525ebd740e35568ca61f9
JHH <j@dorothy-vaughn>
2023-09-08 10:46:25 +0700

    Overwrite by default and added comments.

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #Install nvim as a feature
 sudo apt update
-sudo apt install taskwarrior -y
-cp ~/dotfiles/task/.config/task/taskrc_example ~/.taskrc
-git clone https://github.com/wakatime/vim-wakatime ~/.local/share/nvim/site/pack/plugins/start/wakatime --depth=1
-git clone https://github.com/wakatime/vim-wakatime ~/.vim/pack/plugins/start/wakatime --depth=1
-mkdir -p ~/.local/bin && cd ~/.local/bin
-curl -LO https://github.com/x-motemen/ghq/releases/download/v1.4.2/ghq_linux_amd64.zip 
-unzip ghq_linux_amd64.zip
+sudo apt install taskwarrior -y 
+cp ~/dotfiles/task/.config/task/taskrc_example ~/.taskrc #Initial configuration
+git clone https://github.com/wakatime/vim-wakatime ~/.local/share/nvim/site/pack/plugins/start/wakatime --depth=1 #shallow history
+git clone https://github.com/wakatime/vim-wakatime ~/.vim/pack/plugins/start/wakatime --depth=1 #shallow history
+mkdir -p ~/.local/bin && cd ~/.local/bin #Do both or none
+curl -LO https://github.com/x-motemen/ghq/releases/download/v1.4.2/ghq_linux_amd64.zip  #Follow redirections and save file
+unzip -o ghq_linux_amd64.zip #Overwrite
 cp ghq_linux_amd64/ghq .
 
 
