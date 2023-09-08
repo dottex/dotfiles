09eb51e6fbcf8cef270f02d24f6fb60dbd25d8b8
JHH <j@dorothy-vaughn>
2023-09-08 10:42:33 +0700

    Added ghq for repo managing

--- install.sh
+++ install.sh
@@ -4,5 +4,9 @@ sudo apt install taskwarrior -y
 cp ~/dotfiles/task/.config/task/taskrc_example ~/.taskrc
 git clone https://github.com/wakatime/vim-wakatime ~/.local/share/nvim/site/pack/plugins/start/wakatime --depth=1
 git clone https://github.com/wakatime/vim-wakatime ~/.vim/pack/plugins/start/wakatime --depth=1
+mkdir -p ~/.local/bin && cd ~/.local/bin
+curl -L https://github.com/x-motemen/ghq/releases/download/v1.4.2/ghq_linux_amd64.zip -o ghq.zip
+unzip ghq.zip
+cp ghq/ghq .
 
 
